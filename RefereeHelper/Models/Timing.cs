b57116e1403d60b367a0af0e7492eaf3fdfabdd8
Eugene <evedmedkov4@gmail.com>
2022-11-19 18:37:51 +0300

    Болванка для асинхронного метода получения времени, работа с моделью Timing

--- RefereeHelper/Models/Timing.cs
+++ RefereeHelper/Models/Timing.cs
@@ -9,7 +9,30 @@ namespace RefereeHelper.Models
     public class Timing
     {
         public int Id { get; set; }
-        
+        public bool autoMode { get; set; }
+        public DateTime currentTime { get; set; }
 
+
+         public void GetChipNumber()
+         {
+            Console.WriteLine("Номер чипа: ");
+         } 
+
+         async Task GetCurrentTime()
+        {
+           
+            while (true)
+            { 
+                currentTime = DateTime.Now;
+                 if (autoMode)
+            {
+                await Task.Run(()=> GetChipNumber());
+            }
+            else
+            {
+                continue;
+            }
+            }
+        }
     }
 }
