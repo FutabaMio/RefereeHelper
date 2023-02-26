92537817106b9760be564256487de6ce1a471b5a
FutabaMio <56927381+FutabaMio@users.noreply.github.com>
2023-02-26 13:50:17 +0300

    простейший (не асинхронный) секундомер: -запустить -остановить (без сброса времени)

--- RefereeHelper/Views/TimingView.xaml.cs
+++ RefereeHelper/Views/TimingView.xaml.cs
@@ -5,10 +5,12 @@ using System;
 using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
+using System.Diagnostics;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-
+using System.Windows.Threading;
 
 namespace RefereeHelper.Views
 {
@@ -27,6 +29,8 @@ namespace RefereeHelper.Views
             InitializeComponent();
 
             Loaded+= TimingView_Loaded;
+            dt.Tick+=new EventHandler(dtTick);
+            dt.Interval = new TimeSpan(0, 0, 0, 0, 1);
             LoadEvents();
             
         }
@@ -38,6 +42,40 @@ namespace RefereeHelper.Views
             DataContext = db.Timings.Local.ToObservableCollection();
             TeamTimer.DataContext = db.Timings.Local.ToBindingList();
         }
+
+        //
+        //блок секундомер (не асинхронный)
+        //
+        DispatcherTimer dt = new DispatcherTimer();
+        Stopwatch sw = new Stopwatch();
+        string currentTime = string.Empty;
+
+        void dtTick(object sender, EventArgs e)
+        {
+            if (sw.IsRunning)
+            {
+                TimeSpan ts = sw.Elapsed;
+                currentTime=String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds/10);
+                secundomer.Text = currentTime;
+            }
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            sw.Start();
+            dt.Start();
+        }
+
+        private void TimerStop(object sender, RoutedEventArgs e)
+        {
+            if (sw.IsRunning)
+            {
+                sw.Stop();
+            }
+        }
+        //
+        //блок секундомера конец
+        //
 
         public void LoadEvents()            //надо привести возвращаемые объекты к тексту, придумать, как по названию (или айди выбранного объекта) искать в базе и подгружать участников
         {
@@ -62,44 +100,6 @@ namespace RefereeHelper.Views
         TimeSpan timeOfDifference = new(0, 0, 5);
         //
 
-        System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
-        DateTime currentTime;
-        DateTime startTime;
-        int hours;
-        int minutes;
-        int seconds;
-        int milliseconds;
-        private void dispatcherTimer_Tick(object sender, EventArgs e)
-        {
-            // Изменение отображаемого времени на форме
-
-            hours = (currentTime-startTime).Hours;
-            minutes = (currentTime-startTime).Minutes;
-            seconds = (currentTime-startTime).Seconds;
-            milliseconds = (currentTime-startTime).Milliseconds;
-            currentTime = DateTime.Now;
-
-            secundomer.Content = $"{hours}:{minutes}:{seconds}.{milliseconds}";
-
-            // Forcing the CommandManager to raise the RequerySuggested event
-            CommandManager.InvalidateRequerySuggested();
-        }
-
-        private void TimerStart(object sender, RoutedEventArgs e)
-        {
-                dispatcherTimer.Start(); 
-                dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
-                dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0,1); //обновление информации каждую 0.1 мс
-
-        } 
-
-
-        private void TimerStop(object sender, RoutedEventArgs e)
-        {
-            
-            startTime = DateTime.Now;
-            dispatcherTimer.Stop();
-        }
 
        /* private void StartTimeAccepter_Click(object sender, RoutedEventArgs e) //Потенциальный фикс - асинхронный метод сравнения
         {                                                                      //А лучше подумать, как можно постоянно (или через промежутки времени)
@@ -184,5 +184,7 @@ namespace RefereeHelper.Views
             //почитать, как при записи в бд подсосать данные из таблиц по номеру участнику
             //db.SaveChanges();
         }
+
+       
     }
 }
