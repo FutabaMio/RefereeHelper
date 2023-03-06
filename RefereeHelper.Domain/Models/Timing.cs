7da7c36013f2e3fe9fcb5e006777cec5ee01d4ae
susal <susal@DESKTOP-8PBSDG0>
2023-03-07 00:36:14 +0300

    функция добавления

--- RefereeHelper.Domain/Models/Timing.cs
+++ RefereeHelper.Domain/Models/Timing.cs
@@ -12,13 +12,13 @@ namespace RefereeHelper.Models
     {
         public virtual Start? Start { get; set; }
         public TimeOnly TimeNow { get; set; }
-        public TimeOnly TimeFromStart { get; set; }            // может быть и DateTime
-        public TimeOnly CircleTime { get; set; }
-        public int Circle { get; set; }
-        public int Place { get; set; }
-        public int PlaceAbsolute { get; set; }
+        public TimeOnly? TimeFromStart { get; set; }            // может быть и DateTime
+        public TimeOnly? CircleTime { get; set; }
+        public int? Circle { get; set; }
+        public int? Place { get; set; }
+        public int? PlaceAbsolute { get; set; }
         //public bool autoMode { get; set; } = false;            //почитать об инициализации переменной после указания её свойств
-        public TimeOnly currentTime { get; set; }              //надо распарсить только время, поискать как (TimeOnly currentTime = DateOnly.FromDateTime(timer))
+        public TimeOnly? currentTime { get; set; }              //надо распарсить только время, поискать как (TimeOnly currentTime = DateOnly.FromDateTime(timer))
      }
 }
 
