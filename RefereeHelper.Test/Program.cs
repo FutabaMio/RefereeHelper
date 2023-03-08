2aaff5215568fc3428bbadb532cc36287ded82f1
susal <susal@DESKTOP-8PBSDG0>
2023-03-08 03:26:40 +0300

    создал проект для тестирования GenericDataService

---
+++ RefereeHelper.Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using RefereeHelper.EntityFramework;
+using RefereeHelper.EntityFramework.Services;
+using RefereeHelper.Models;
+
+/*using (var dbContext = new RefereeHelperDbContextFactory().CreateDbContext())
+{
+    dbContext.Set<Discharge>().Add(new Discharge { Name = "228"});
+
+    dbContext.SaveChanges();
+}*/
+
+GenericDataService<Discharge> genericDataService = new(new RefereeHelperDbContextFactory());
+genericDataService.Create(new Discharge() { Name = "220 Вольт" });
+var discharges = genericDataService.GetAll().Result;
+foreach (var discharge in discharges)
+{
+    Console.WriteLine(discharge.Name);
+}
+genericDataService.Update(1, new Discharge() { Name = "228 Вольт" });
+Console.WriteLine(genericDataService.Get(1).Result.Name);
+if (genericDataService.Delete(1).Result)
+{
+    Console.WriteLine("Удалено");
+}
