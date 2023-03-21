bbcc6c49bb26e21b806e4364da730bca9ff06f90
susal <susal@DESKTOP-8PBSDG0>
2023-03-22 01:05:03 +0300

    появилось добавление времени круга

--- RefereeHelper.EntityFramework/Services/IDataService.cs
+++ RefereeHelper.EntityFramework/Services/IDataService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace RefereeHelper.EntityFramework.Services
     public interface IDataService<T>
     {
         Task<IEnumerable<T>> GetAll();
+        Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>> predicate);
         Task<T> Get(int id);
         Task<T> Create (T entity);
         Task<T> Update (int id, T entity);
