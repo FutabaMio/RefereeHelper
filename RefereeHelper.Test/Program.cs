92adca5dec644a7ff3da2fbe958af7cad2aaffe7
susal <susal@DESKTOP-8PBSDG0>
2023-03-27 00:55:38 +0300

    незначительные изменения

--- RefereeHelper.Test/Program.cs
+++ RefereeHelper.Test/Program.cs
@@ -11,14 +11,15 @@ using RefereeHelper.Models;
 
 GenericDataService<Discharge> genericDataService = new(new RefereeHelperDbContextFactory());
 genericDataService.Create(new Discharge() { Name = "220 Вольт" });
+genericDataService.Create(new Discharge() { Name = "220 Вольтаж" });
 var discharges = genericDataService.GetAll().Result;
 foreach (var discharge in discharges)
 {
     Console.WriteLine(discharge.Name);
 }
-genericDataService.Update(1, new Discharge() { Name = "228 Вольт" });
-Console.WriteLine(genericDataService.Get(1).Result.Name);
-if (genericDataService.Delete(1).Result)
-{
-    Console.WriteLine("Удалено");
-}
+//genericDataService.Update(1, new Discharge() { Name = "228 Вольт" });
+//Console.WriteLine(genericDataService.Get(1).Result.Name);
+//if (genericDataService.Delete(1).Result)
+//{
+//    Console.WriteLine("Удалено");
+//}
