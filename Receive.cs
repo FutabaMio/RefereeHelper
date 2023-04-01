49f9d72a674c41f7c6b1be817d71a27a468a0bcf
Misha <mi035@mail.ru>
2023-04-01 16:47:21 +0300

    Изменения тестового приложения

---
+++ Receive.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Sockets;
+
+public class UdpReceive
+{
+    public TimeOnly time;
+    UdpClient udpClient = new UdpClient(27069);
+    UdpReceiveResult result;
+    byte[] datagram;
+    string received;
+    int secondOfDifference = 5;
+    timeOfDifference = new (0, 0, secondOfDifference);
+    
+	public UdpReceive(int port)
+	{
+		
+	}
+    async Task<string> Receive()
+    {
+        result = await udpClient.ReceiveAsync();
+        datagram = result.Buffer;
+        received = Encoding.UTF8.GetString(datagram);
+        time = TimeOnly.FromDateTime(DateTime.Now);
+        received = received.Substring(received.IndexOf("Tag:") + 4);
+        received = received.Substring(0, received.IndexOf(" "));
+    }
+}
+public class Processing
+{
+    public Processing()
+    {
+
+    }
+}
--- RefereeHelper.Test/Program.cs
+++ RefereeHelper.Test/Program.cs
@@ -1,14 +1,258 @@
-﻿using RefereeHelper.EntityFramework;
+﻿using RefereeHelper;
+using RefereeHelper.EntityFramework;
 using RefereeHelper.EntityFramework.Services;
 using RefereeHelper.Models;
-using System.Runtime.CompilerServices;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
-
-IEnumerable<Member> ms;
-var discharge = new Discharge { Name = "228" };
-using (var dbContext = new RefereeHelperDbContextFactory().CreateDbContext())
+int countOfStartingPeople = 0;
+string[] codes = { "Первичная обработка данных", "Сбор", "Заполнить БД", "Проверить БД", "help", "close"};
+decimal CountOfLapsForHim;
+int CountOfFinishingPeople = 0;
+TimeSpan timeOfDifference = new TimeSpan(0, 0, 5);
+while (true)
 {
-    dbContext.Set<Member>().Add(new Member
+    Console.WriteLine("Введите запрос операции:");
+    string code = Console.ReadLine();
+    Console.WriteLine($"\n\n\t\t{code}\n");
+    switch (code)
+    {
+        case "Первичная обработка данных":
+            First();
+            break;
+        case "Сбор":
+            Second();
+            break;
+        case "Заполнить БД":
+            Third();
+            break;
+        case "Проверить БД":
+            CheckDB();
+            break;
+        case "help":
+            GiveInfo();
+            break;
+        case "close":
+            break;            
+        default:
+            GiveInfo();
+            Console.WriteLine("TryNow");
+            break;
+    }
+}
+void First()
+{
+
+    countOfStartingPeople = 0;
+    while (true)
+    {        
+        Console.WriteLine("Введите команду (<<РВВ>> - разница во времени, по стандарту 5 секунд; <<демо>> для демонстрации):");
+        string code = Console.ReadLine();
+
+        switch (code)
+        {
+            case "РВВ":
+                RVV(Console.ReadLine());
+                break;
+            case "демо":
+                Demo();
+                break;
+            default:break;
+        }       
+        
+    }
+}
+TimeSpan RVV(string k)
+{
+    try
+    { 
+        return new(0, 0, Int32.Parse(k));
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"\n\n-----------------------------------------------------------------------------{e.Message}-----------------------------------------------------------------------------\n");
+    }
+    return new(0, 0, 5);
+}
+void Demo()
+{
+    Process("Xi");
+    Process("P4BK");
+    Process("Xi");
+    Process("P4BK");
+    Console.WriteLine("Имя\tТекущее время\tВремя со старта\tКруг\tПозиция\tВремя круга");
+}
+void CheckDB()
+{
+    var dbContext = new RefereeHelperDbContextFactory().CreateDbContext();
+    
+    var st = dbContext.Set<Start>().Select(x => new Start
+    {
+        Partisipation = new Partisipation
+        {
+            Competition = x.Partisipation.Competition,
+            Group = new Group
+            {
+                Name = x.Partisipation.Group.Name,
+                Distance = x.Partisipation.Group.Distance
+            },
+            Member = new Member
+            {
+                Name = x.Partisipation.Member.Name,
+                Discharge = new Discharge
+                {
+                    Name = x.Partisipation.Member.Discharge.Name
+                },
+                Club = new Club
+                {
+                    Name = x.Partisipation.Member.Club.Name,
+                    Region = new Region
+                    {
+                        Name = x.Partisipation.Member.Club.Region.Name
+                    }
+                }
+            }
+        },
+        Number = x.Number,
+        Chip = x.Chip,
+        StartTime = x.StartTime
+    }).ToList();
+    Console.WriteLine($"ИМЯ\tНомер\tЧип\tИмя группы\tИмя дистанции\t\tСтартовое время\tРазряд\t\tКлуб\t\tРегион");
+    foreach (var s in st)
+    {
+        Console.WriteLine($"{s.Partisipation?.Member?.Name}" +
+                        $"\t{s.Number}" +
+                        $"\t{s.Chip}" +
+                        $"\t{s.Partisipation?.Group?.Name}" +
+                        $"\t\t{s.Partisipation?.Group?.Distance.Name}" +
+                        $"\t{s.StartTime}" +
+                        $"\t\t{s.Partisipation?.Member?.Discharge?.Name}" +
+                        $"\t{s.Partisipation?.Member?.Club?.Name}" +
+                        $"\t{s.Partisipation?.Member?.Club?.Region?.Name}");
+    }
+
+}
+
+void GiveInfo()
+{
+    for(int i = 0; i < codes.Length; i++)
+    {
+        Console.WriteLine($"\t-{codes[i]}");
+    }
+}
+void Process(string received)
+{
+    Processing p = new();
+    GenericDataService<Timing> DataService = new(new RefereeHelperDbContextFactory());
+    if (p.dbContext.Set<Timing>().Any(x => x.Start?.Chip == received))
+    {
+        countOfStartingPeople++;
+        p.dbContext.Set<Timing>().Add(new Timing
+        {
+            TimeNow = TimeOnly.FromDateTime(DateTime.Now),
+            Start = p.dbContext.Set<Start>().First(x => x.Chip == received)
+        });
+        var t =
+
+        CountOfLapsForHim = p.dbContext.Set<Timing>().First(x => x.Id == t.Id).Start.Partisipation.Group.Distance.Circles;
+        t.TimeFromStart = TimeOnly.FromTimeSpan((TimeSpan)(t.Start.StartTime - t.TimeNow));
+        t.Circle = p.GetOfLapsForHim(t.Id);
+        t.CircleTime = p.GetTimeOfLap(t.Id);
+        t.IsFinish = p.GetIsFinish(t.Id);
+        p.RefrechPlace(t.Id);
+        p.RefrechAbsolutePlace(t.Id);
+        DataService?.Update(t.Id, t);
+    }
+    else
+    {
+
+        for (int i = DataService.GetAll().Result.Count() - 1; i > -1; i--)
+        {
+
+            if (DataService.Get(i).Result.Start?.Chip == received)
+            {
+                if (TimeOnly.FromDateTime(DateTime.Now) - DataService.Get(i).Result.TimeNow > timeOfDifference)
+                {
+                    var t = DataService.Create(new Timing()
+                    {
+                        TimeNow = TimeOnly.FromDateTime(DateTime.Now),
+                        Start = p.dbContext.Set<Start>().First(x => x.Chip == received)
+                    }).Result;
+                    t.TimeFromStart = TimeOnly.FromTimeSpan((TimeSpan)(t.Start?.StartTime - t.TimeNow));
+                    CountOfLapsForHim = DataService.Get(t.Id).Result.Start.Partisipation.Group.Distance.Circles;
+                    t.Circle = p.GetOfLapsForHim(t.Id);
+                    t.CircleTime = p.GetTimeOfLap(t.Id);
+                    t.IsFinish = p.GetIsFinish(t.Id);
+                    if (t.IsFinish.Value)
+                    {
+                        CountOfFinishingPeople++;
+                    }
+                    p.RefrechPlace(t.Id);
+                    p.RefrechAbsolutePlace(t.Id);
+                    DataService?.Update(t.Id, t);
+                }
+                break;
+            }
+        }
+
+    }
+}
+
+void Second()
+{
+    Console.Write("Введите номер:");
+    string msg = Console.ReadLine();
+    UDPReceive u = new(27069);
+    
+    string receive;
+    //Send(msg);
+    //Console.WriteLine(receive);
+    int i = 0, k = 10;
+    while (i<k)
+    {
+        i++;
+        try
+        {
+            receive = u.Receive().Result.ToString();
+            if (receive == msg)
+            {
+                Console.WriteLine($"\tУспешно\n<<{receive}>> равно <<{msg}>>");
+            }
+            else
+            {
+                Console.WriteLine($"\tБезуспешно\n<<{receive}>> не равно <<{msg}>>");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        
+    }
+    u.Close();
+    
+
+}
+void Third()
+{
+    Region region = new()
+    {
+        Name = "Владимир",
+        СodeNumber = 33
+    };
+    Club club = new()
+    {
+        Region = region,
+        Couch = "Пригожин Евгений Викторович",
+        Name = "Ватные палочки"
+    };
+    Discharge discharge = new()
+    {
+        Name = "1-ый разряд"
+    };
+    Member Me = new() 
     {
         Name = "Андрей",
         FamilyName = "Сусалев",
@@ -17,15 +261,103 @@ using (var dbContext = new RefereeHelperDbContextFactory().CreateDbContext())
         City = "Владимир",
         BornDate = new DateTime(2004, 6, 1),
         Gender = true,
-        Discharge = discharge
-    });
-    ms = dbContext.Members.Where(x => x.Name == "Андрей");
-    dbContext.SaveChanges();
+        Discharge = discharge,
+        Club = club
+    };
+    Member Michle = new()
+    {
+        Name = "Михаил",
+        FamilyName = "Щукин",
+        SecondName = "Сергеевич",
+        Phone = "89542281488",
+        City = "Владимир",
+        BornDate = new DateTime(2003, 10, 19),
+        Gender = true,
+        Discharge = discharge,
+        Club = club
+    };
+    Distance distance = new()
+    {
+        Name = "Общага -> Мегаторг",
+        Length = 3000,
+        Height = 0,
+        Circles = 2,
+        StartTime = new TimeOnly(12, 00, 00)
+    };
+    Group group = new()
+    {
+        Distance = distance,
+        Gender = true,
+        Name = "Палочки",
+        StartAge = 18,
+        EndAge = 54
+    };
+    Competition competition = new()
+    {
+        Name = "Взятие ТЦ",
+        Organizer = "Пу",
+        Place = "Владимир",
+        Date = new DateTime(2023,3,30),
+        Judge = "Джо",
+        Secretary = "Злнск",
+        TypeAge = true
+    };
+    Partisipation partisipationMe = new()
+    {
+        Competition = competition,
+        Member = Me,
+        Group = group
+    };
+    Partisipation partisipationMichle = new()
+    {
+        Competition = competition,
+        Member = Michle,
+        Group = group
+    };
+    Team team = new()
+    {
+        Name = "Крутые"
+    };
+    Start startMe = new()
+    {
+        Partisipation = partisipationMe,
+        Team = team,
+        StartTime = distance.StartTime,
+        Number = 88,
+        Chip = "P4BK"
+    };
+    Start startMichle = new()
+    {
+        Partisipation = partisipationMichle,
+        Team = team,
+        StartTime = distance.StartTime,
+        Number = 54,
+        Chip = "Xi"
+    };
+    using (var dbContext = new RefereeHelperDbContextFactory().CreateDbContext())
+    {
+        dbContext.Set<Start>().Add(startMe);
+        dbContext.Set<Start>().Add(startMichle);
+        dbContext.SaveChanges();
+    }
 }
-foreach (var m in ms)
+async Task<string> Send(string msg)
 {
-    Console.WriteLine($"{m.Id}-{m.Name}-{m.Discharge?.Name}");
-}
+    var listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+    byte[] data = Encoding.UTF8.GetBytes(msg);
+    EndPoint remotePoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 27069);
+
+    SocketAsyncEventArgs e = new SocketAsyncEventArgs();
+    Console.WriteLine($"Отправлено. \n\t<<Disc:2000/01/01 00:00:04, Last:2000/01/01 00:00:04, Count:00001, Ant:01, Type:04, Tag:{msg}        >>");
+    await listeningSocket.SendToAsync(data, SocketFlags.None, remotePoint);
+    Console.WriteLine($"Закончена отправка.\n\t\t{DateTime.Now}");
+    return "Успешная отправка";
+} 
+
+
+
+
+
 /*var competition = competitionDataService.Create(
         new Competition
         {
