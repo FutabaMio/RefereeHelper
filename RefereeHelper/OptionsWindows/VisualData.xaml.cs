7cdba1f6f95ed32e70e823ac605ec4daad03d999
King <kenkebashvilii@mail.ru>
2023-05-13 20:03:31 +0300

    Изменил визуализацию данных, добавил колонку с информацией

--- RefereeHelper/OptionsWindows/VisualData.xaml.cs
+++ RefereeHelper/OptionsWindows/VisualData.xaml.cs
@@ -36,7 +36,7 @@ namespace RefereeHelper.OptionsWindows
         }
 
         Competition competition;
-        int cilcle = 1;
+        int cilcle = 1, cilcle1  = 1;
 
         public VisualData(bool Type, Competition c)
         {
@@ -57,13 +57,21 @@ namespace RefereeHelper.OptionsWindows
                 foreach (var distance in distances)
                 {
                     DistanceCMB.Items.Add(distance);
+                    Distance1CMB.Items.Add(distance);
                 }
                 DistanceCMB.SelectedIndex = 0;
+                Distance1CMB.SelectedIndex = 0;
                 System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
 
                 timer.Tick += new EventHandler(FillDistance);
                 timer.Interval = new TimeSpan(0, 0, 1);
                 timer.Start();
+
+                System.Windows.Threading.DispatcherTimer timer1 = new System.Windows.Threading.DispatcherTimer();
+
+                timer1.Tick += new EventHandler(FillDistance1);
+                timer1.Interval = new TimeSpan(0, 0, 1);
+                timer1.Start();
             }
         }
 
@@ -133,6 +141,77 @@ namespace RefereeHelper.OptionsWindows
                             if (timing.Start?.Number != null)
                                 dataItem.Number = timing.Start?.Number.ToString();
                             MainDB.Items.Add(dataItem);
+                        }
+                    }
+                }
+            }
+        }
+        private void FillDistance1(object sender, EventArgs e)
+        {
+            var dbContext = new RefereeHelperDbContextFactory().CreateDbContext();
+
+            TimeOnly buf;
+
+            Main1DB.Items.Clear();
+
+            var timings = dbContext.Set<Timing>().Select(x => new Timing
+            {
+                Id = x.Id,
+                TimeFromStart = x.TimeFromStart,
+                IsFinish = x.IsFinish,
+                Circle = x.Circle,
+                Start = new Start
+                {
+                    Id = x.Start.Id,
+                    Partisipation = new Partisipation
+                    {
+                        Id = x.Start.Partisipation.Id,
+                        CompetitionId = x.Start.Partisipation.CompetitionId,
+                        Group = new Group
+                        {
+                            DistanceId = x.Start.Partisipation.Group.DistanceId
+                        },
+                        Member = new Member
+                        {
+                            Name = x.Start.Partisipation.Member.Name,
+                            FamilyName = x.Start.Partisipation.Member.FamilyName
+                        }
+                    },
+                    Number = x.Start.Number
+                }
+            }).ToList();
+
+            DataItems dataItem;
+
+            Distance distance = (Distance)Distance1CMB.SelectedItem;
+            if (distance != null)
+            {
+                foreach (Timing timing in timings)
+                    if (timing.Start?.Partisipation.Group?.DistanceId == distance.Id)
+                        if (timing.Circle > cilcle1 && timing.Circle != null)
+                        {
+                            cilcle1 = (int)timing.Circle;
+                            break;
+                        }
+                foreach (Timing timing in timings)
+                {
+                    if (timing.Start?.Partisipation.Group?.DistanceId == distance.Id)
+                    {
+                        if (timing.Circle == cilcle1)
+                        {
+                            dataItem = new DataItems();
+                            if (timing.TimeFromStart != null)
+                            {
+                                buf = (TimeOnly)timing.TimeFromStart;
+                                dataItem.TimeFromStart = buf.ToShortTimeString();
+                            }
+                            if (timing.Start?.Partisipation?.Member?.Name != null)
+                                dataItem.Name = timing.Start?.Partisipation?.Member?.Name;
+                            if (timing.Start?.Partisipation?.Member?.FamilyName != null)
+                                dataItem.FamalyName = timing.Start?.Partisipation?.Member?.FamilyName;
+                            if (timing.Start?.Number != null)
+                                dataItem.Number = timing.Start?.Number.ToString();
+                            Main1DB.Items.Add(dataItem);
                         }
                     }
                 }
